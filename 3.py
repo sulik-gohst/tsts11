4a968a5f588294865573f1f6349c65a0c9b609b2
sulik-gohst <myrzakanovsultan0@gmail.com>
2020-07-17 14:28:11 +0600

    123

---
+++ 3.py
@@ -0,0 +1,20 @@
+11 == 11 and 2 == 1          #False
+"compare" == "compare"        #True                                
+11 == 11 or 22 != 100          True
+True and True                   #True
+False and True                  False
+True and 3 == 3                 True
+False and 42 != 42              False
+True or 13 == 13                True
+"swimming" == "testing"          False
+"tester" != "testing"              False
+"test" == 123               False
+12 != 0 and 22 == 1            False
+not (True and False)            True
+not (1 == 1 and 0 != 1)         False
+not (105 == 4 or 101 == 101)        True
+not (11 != 101 or 7 == 4)       True
+not ("testing" == "testing" and "Ted" == "Fool Guy")        True
+8 == 4 and not ("wrestling" == 1 or 4 == 0)        False
+"spam" == "bacon" and not (3 == 4 or 7 == 7)    False
+6 == 6 and not ("tdd" == "tdd" or "Python" == "Super")  True
