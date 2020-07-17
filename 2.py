10bf1a5bc308f991f67ff624b36cfde333f98a61
sulik-gohst <myrzakanovsultan0@gmail.com>
2020-07-17 14:25:25 +0600

    123

---
+++ 2.py
@@ -0,0 +1,9 @@
+given_number = int(input())
+if given_number % 3 and 5 == 0:
+    print("HahaHoo!")
+elif given_number % 3 == 0:
+    print("Haha")
+elif given_number % 5 == 0:
+    print('Hoo')
+else:
+    print("Aaaa")
